15935b54d01f537f7c69008c57553eab5f82db4b
RivaldoJR <113871228+Grvld27@users.noreply.github.com>
2025-07-12 16:39:31 +0800

    fix viteconfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,47 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { VitePWA } from 'vite-plugin-pwa';
+
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: [
-        'favicon.ico', 
-        'robots.txt', 
-        'pwa-192x192.png', 
-        'pwa-512x512.png'
-      ],
-      manifest: {
-        name: 'My App',
-        short_name: 'App',
-        description: 'A Vite React PWA App',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-      workbox: {
-        navigateFallback: '/index.html', // 💡 Kunci penting untuk <BrowserRouter>
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-      },
-      devOptions: {
-        enabled: false,
-      },
-    }),
+ 
   ],
   resolve: {
     alias: {
