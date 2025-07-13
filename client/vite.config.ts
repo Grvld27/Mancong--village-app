699ba1abcf30c1ad49c08dfa286d3334a31bda55
RivaldoJR <113871228+Grvld27@users.noreply.github.com>
2025-07-13 21:39:22 +0800

    fix viteconfig.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-  plugins: [
-    react(),
- 
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
   },
   server: {
@@ -18,10 +18,12 @@ export default defineConfig({
       '/api': 'http://localhost:5000',
     },
   },
-build:{
-
-  outDir:'dist',
+ build: {
+  outDir: 'dist',
+  minify: false,
+  // optional tambahan
+  rollupOptions: {
+    treeshake: false,
+  },
 }
-
-
 });
