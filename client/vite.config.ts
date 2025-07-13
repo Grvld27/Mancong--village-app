84b04cfa7474db03c9afb6fd27f7a082ff8cb34c
RivaldoJR <113871228+Grvld27@users.noreply.github.com>
2025-07-14 07:32:20 +0800

    fix vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  // base: '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
